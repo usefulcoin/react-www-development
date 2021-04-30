a745390cb41a4f131ce30e0d821765d8dc8f7c0b
munair <munair@gmail.com>
2021-04-30 19:20:24 +0900

    fingers crossed

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class Blog extends React.Component {
+  componentDidMount() {
+    window.mediumWidget();
+  }
+  render() {
+    return ();
+  }
+
   render() {
   return (
     <div>
       {/* Start Content */}
         <h1 className="major" id="blog">Medium Publication</h1>
-        <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> reflects our views on the industry:</p>
+        <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> occasionally reflects our views on the industry or aspects of an engagment. Take a look:</p>
+        <div id="medium-widget"></div>
         <p>Please follow the publication if you subscribe to our views.</p>
       {/* End Content */}
     </div>
