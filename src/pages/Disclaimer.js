53d2f2e8b794c0206beced87623bc23d3620dbf3
munair <munair@gmail.com>
2021-04-30 18:35:08 +0900

    add disclaimer

---
+++ src/pages/Disclaimer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Trade extends React.Component {
+  render() {
+  return (
+    <div>
+      {/* Start Content */}
+        <h1 className="major" id="blog">Medium Publication</h1>
+        <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> reflects our views on the industry:</p>
+        <p>Please follow the publication if you subscribe to our views.</p>
+      {/* End Content */}
+    </div>
+  );
+  }
+}
+
+export default Trade;
