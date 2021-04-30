01891c9380c12b5afcdf07fcec8895f46d13d734
munair <munair@gmail.com>
2021-04-30 21:03:20 +0900

    add cointelegraph publications

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -6,7 +6,22 @@ class Publications extends React.Component {
     <div>
       {/* Start Content */}
         <h1 className="major" id="publications">Publications</h1>
-        <p>Coming soon.</p>
+        <h3>Coin Telegraph</h3>
+        <p>
+          <a href="https://cointelegraph.com/news/ico-token-rewards-policy-can-have-unintended-consequences-pose-dilemma-expert-blog">
+            https://cointelegraph.com/news/ico-token-rewards-policy-can-have-unintended-consequences-pose-dilemma-expert-blog
+          </a>
+        </p>
+        <p>
+          <a href="https://cointelegraph.com/news/correlation-between-cryptocurrency-value-and-exchange-listings-expert-blog">
+            https://cointelegraph.com/news/correlation-between-cryptocurrency-value-and-exchange-listings-expert-blog
+          </a>
+        </p>
+        <p>
+          <a href="https://cointelegraph.com/news/korea-s-unique-ability-to-make-its-reverse-ico-fad-permanent-expert-take">
+            https://cointelegraph.com/news/korea-s-unique-ability-to-make-its-reverse-ico-fad-permanent-expert-take
+          </a>
+        </p>
       {/* End Content */}
     </div>
   );
