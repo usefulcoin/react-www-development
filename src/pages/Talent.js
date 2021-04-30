c59fc152f7ae7ef625254d9f27cebed6adf65c6e
munair <munair@gmail.com>
2021-04-30 19:23:58 +0900

    remove violations

--- src/pages/Talent.js
+++ src/pages/Talent.js
@@ -13,14 +13,14 @@ class Talent extends React.Component {
             <img src="images/linkedin-usefulcoin-principal-advisor-350x350px-1.png" alt="munair" />
           </a>
         </span>
-        <p>After completing his first token marketing campaign in Seoul, Korea in early 2017, <a href="https://www.linkedin.com/in/munair/" target="_blank" rel="noopener">Munair Simpson</a> established Useful Coin LLC to continue to help new ventures strategically develop tokens and delve deeper into the cryptocurrency world.</p>
+        <p>After completing his first token marketing campaign in Seoul, Korea in early 2017, <a href="https://www.linkedin.com/in/munair/" target="_blank" rel="noreferrer">Munair Simpson</a> established Useful Coin LLC to continue to help new ventures strategically develop tokens and delve deeper into the cryptocurrency world.</p>
         <p>Today, Munair is completely committed to researching and developing useful cryptocurrencies. In particular, he enjoys advising ventures that choose to finance their aspirations issuing fundamentally valuable tokens. Munair then introduces those coins to a network of cryptocurrency funds and private investors.</p>
         <p>Having the good fortune to research, develop, and implement strategies to successfully capitalize several new token-financed ventures, Munair now provides advisory services to clients around the globe, including South Korea, the Cayman Islands, Jamaica, and the United States of America.</p>
         <p>Munair graduated from the Wharton School at the University of Pennsylvania with an MBA in marketing. He also studied the analysis, design, and management of information systems at the University of California in Berkeley and received a bachelor of science in industrial engineering from the University of the West Indies in Trinidad and Tobago.</p>
-        <p>When time permits, Munair enjoys contributing to <a href="http://coincentral.com/author/munair/">Coin Central</a> and the Expert Take at <a href="http://cointelegraph.com/authors/munair_simpson">Coin Telegraph</a>.</p>
+        <p>When time permits, Munair enjoys contributing to <a href="http://coincentral.com/author/munair/"  target="_blank" rel="noreferrer">Coin Central</a> and the Expert Take at <a href="http://cointelegraph.com/authors/munair_simpson"  target="_blank" rel="noreferrer">Coin Telegraph</a>.</p>
         <h3 className="major" id="mason">Mason Eckenrod</h3>
         <span className="image left">
-          <a className="image dropshadow" href="https://www.linkedin.com/in/munair/">
+          <a className="image dropshadow" href="https://www.linkedin.com/in/munair/"  target="_blank" rel="noreferrer">
             <img src="images/mason-350x350px.png" alt="mason" />
           </a>
         </span>
