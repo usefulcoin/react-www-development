63647f3813f969218564d988708754ffa4b3e471
munair <munair@gmail.com>
2021-04-30 14:58:32 +0900

    need new fonts

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ class Intro extends React.Component {
       {/* Start Intro */}
       <div id="intro">
         <img src="images/usefulcoin-whiteicon-128x128px.png" width="75px" height="75px" alt="white icon" />
-        <h1>USEFUL COIN</h1>
+        <h2>USEFUL COIN</h2>
         <p>research collaboration & strategic engagment</p>
         <ul className="actions">
           <li><a href="#header" className="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
