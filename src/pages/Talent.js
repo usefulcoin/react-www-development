a53df3cd91e253c502f48ed7308210a5c02795df
munair <munair@gmail.com>
2021-04-30 17:00:59 +0900

    try that.

--- src/pages/Talent.js
+++ src/pages/Talent.js
@@ -7,15 +7,23 @@ class Trade extends React.Component {
       {/* Start Content */}
         <h1 className="major" id="talent">Talent</h1>
         <h3 className="major" id="munair">Munair Simpson</h3>
+        <span className="image left">
+          <a className="image dropshadow" href="https://www.linkedin.com/in/munair/">
+            <img src="https://usefulcoin.files.wordpress.com/2018/12/linkedin-usefulcoin-principal-advisor-350x350px-1.png?w=175&amp;h=175" alt="munair" width="175" height="175" />
+          </a>
+        </span>
         <p>After completing his first token marketing campaign in Seoul, Korea in early 2017, <a href="https://www.linkedin.com/in/munair/" target="_blank" rel="noopener">Munair Simpson</a> established Useful Coin LLC to continue to help new ventures strategically develop tokens and delve deeper into the cryptocurrency world.</p>
         <p>Today, Munair is completely committed to researching and developing useful cryptocurrencies. In particular, he enjoys advising ventures that choose to finance their aspirations issuing fundamentally valuable tokens. Munair then introduces those coins to a network of cryptocurrency funds and private investors.</p>
         <p>Having the good fortune to research, develop, and implement strategies to successfully capitalize several new token-financed ventures, Munair now provides advisory services to clients around the globe, including South Korea, the Cayman Islands, Jamaica, and the United States of America.</p>
-        <p><a className="image dropshadow" href="https://www.linkedin.com/in/munair/"><img src="https://usefulcoin.files.wordpress.com/2018/12/linkedin-usefulcoin-principal-advisor-350x350px-1.png?w=175&amp;h=175" alt="munair" width="175" height="175" /></a></p>
         <p>Munair graduated from the Wharton School at the University of Pennsylvania with an MBA in marketing. He also studied the analysis, design, and management of information systems at the University of California in Berkeley and received a bachelor of science in industrial engineering from the University of the West Indies in Trinidad and Tobago.</p>
         <p>When time permits, Munair enjoys contributing to <a href="http://coincentral.com/author/munair/">Coin Central</a> and the Expert Take at <a href="http://cointelegraph.com/authors/munair_simpson">Coin Telegraph</a>.</p>
         <h3 className="major" id="mason">Mason Eckenrod</h3>
+        <span className="image left">
+          <a className="image dropshadow" href="https://www.linkedin.com/in/munair/">
+            <img src="https://usefulcoin.files.wordpress.com/2021/04/mason-350x350px.png?w=175&amp;h=175" alt="mason" width="175" height="175" />
+          </a>
+        </span>
         <p>Mason is an aspiring computer scientist. He is currently working on earning his Bachelor’s of Computer Science at the University of Texas in San Antonio.</p>
-        <p><a className="image dropshadow" href="https://www.linkedin.com/in/munair/"><img src="https://usefulcoin.files.wordpress.com/2021/04/mason-350x350px.png?w=175&amp;h=175" alt="mason" width="175" height="175" /></a></p>
         <p>During his studies, Mason discovered cryptocurrencies and became completely enthralled with its allure of investing, trading, and decentralization. During his free time, Mason enjoys writing scripts and spending time with his weim reiner.</p>
       {/* End Content */}
     </div>
