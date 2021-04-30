85fd870032354c696fd82207b13b5ba719b0daa6
munair <munair@gmail.com>
2021-04-30 17:22:00 +0900

    testing new layout for consistency

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,21 +6,18 @@ class About extends React.Component {
   return (
     <div>
       {/* Start Content */}
-        <article className="post featured">
-          <header className="major">
-            <span className="date"><img src="images/usefulcoin-blackicon-250x250px.png" width="10%" alt="brand icon" /></span>
-            <h1 class="major" id="about">RESEARCH & STRATEGY</h1>
-            <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
-              research and business development. That said, we have a penchant for collaborating teams in the cryptocurrency industry. Most of
-              our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
-              engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just give us <a href="tel:+12062270634">
-              call</a>.</p>
-          </header>
-          <ul className="actions special">
-            <li><a href="mailto:info@usefulcoin.com" className="button large">Email Us</a></li>
-          </ul>
-          <p />
-        </article>
+        <h1 class="major" id="about">RESEARCH & STRATEGY</h1>
+        <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
+          research and business development. That said, we have a penchant for collaborating teams in the cryptocurrency industry. Most of
+          our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
+          engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just give us <a href="tel:+12062270634">
+          call</a>.</p>
+        <ul className="actions special">
+          <li>
+            <a href="mailto:info@usefulcoin.com" className="button large">Email Us</a>
+          </li>
+        </ul>
+        <span className="date"><img src="images/usefulcoin-blackicon-250x250px.png" width="5%" alt="brand icon" /></span>
       {/* End Content */}
     </div>
   );
