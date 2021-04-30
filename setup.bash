0fb1616e4ed4f6738664bebdeea1389c34589935
munair <munair@gmail.com>
2021-04-30 13:52:04 +0900

    update for yarn

--- setup.bash
+++ setup.bash
@@ -2,8 +2,8 @@
 #
 # script name: setup.bash
 # script author: munair simpson
-# script created: 20200624
-# script purpose: spin up node/express with Pixelarity's Linear template
+# script created: 20210430
+# script purpose: spin up node/express with Pixelarity's Massively template
 
 # disable/enable debugging.
 debug="false" && echo [$0] set debug mode to "$debug".
@@ -15,13 +15,15 @@ sudo apt -y install unzip > /dev/null 2>&1 && echo [$0] installed unzip APT.
 sudo apt -y install npm > /dev/null 2>&1 && echo [$0] installed NPM APT.
 sudo apt -y install vim > /dev/null 2>&1 && echo [$0] installed vim APT.
 
-# step 2: install Web3Modal React Example.
-if $debug ; then sudo npm install ; fi
-sudo npm install > /dev/null 2>&1 && echo [$0] installed Express Generator.
+# step 2: install Yarn.
+if $debug ; then sudo npm install --global yarn; fi
+sudo npm install --global yarn > /dev/null 2>&1 && echo [$0] installed Yarn.
 
 # step 3: verify the installation of APTs.
 nodeversion=$(nodejs --version) && echo [$0] verified the installation of nodejs version $nodeversion.
 npmversion=$(npm --version) && echo [$0] verified the installation of npm version $npmversion.
+yarnversion=$(yarn --version) && echo [$0] verified the installation of yarn version $yarnversion.
 
-# step 4: start React app.
-# npm start
+# step 4: yarn up.
+if $debug ; then sudo yarn && yarn start ; fi
+sudo yarn install > /dev/null 2>&1 && echo [$0] yarned up.
