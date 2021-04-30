abb7bfbb0e07b4ec3066267954a081392adb541e
munair <munair@gmail.com>
2021-04-30 18:29:01 +0900

    final edit. i hope.

--- src/pages/Engagements.js
+++ src/pages/Engagements.js
@@ -8,8 +8,8 @@ class Trade extends React.Component {
         <h1 className="major" id="engagements">Engagements</h1>
         <p>All-hands-on-deck <b>engagements</b> lasting three to nine months are our specialty. Although we
            draft and execute thoughful marketing strategies, write impactful press releases, exhibit at major
-           conferences, and develop educational content, engagements allow us to leverage our various talents
-           and produce the greatest impact.</p>
+           conferences, and develop educational content on demand, engagements really allow us to leverage our
+           various talents and produce the greatest impact.</p>
         <span className="image left">
           <a className="image dropshadow" href="https://www.biopset.com/">
             <img src="images/biop-combination-mark-250x150px.png" alt="biop-combination-mark" />
