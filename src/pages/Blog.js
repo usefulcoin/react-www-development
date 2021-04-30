7bc253de2b0ecff3c85176040ff7c4ed6afd8139
munair <munair@gmail.com>
2021-04-30 14:36:29 +0900

    shuffle

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Trade extends React.Component {
+  MediumWidget.Init({
+    renderTo: "#medium-widget",
+    params: {
+      resource: "https://medium.com/usefulcoin",
+      postsPerLine: 2,
+      limit: 4,
+      picture: "big",
+      fields: ["description", "author", "claps", "publishAt"],
+      ratio: "landscape"
+    }
+  })
   render() {
   return (
     <React.Fragment>
@@ -8,17 +19,6 @@ class Trade extends React.Component {
         <h1 class="major" id="blog">Medium Publication</h1>
         <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> reflects our views on the industry:</p>
         <div id="medium-widget">
-          MediumWidget.Init({
-            renderTo: "#medium-widget",
-            params: {
-              resource: "https://medium.com/usefulcoin",
-              postsPerLine: 2,
-              limit: 4,
-              picture: "big",
-              fields: ["description", "author", "claps", "publishAt"],
-              ratio: "landscape"
-            }
-          })
         </div>
         <p>Please follow the publication if you subscribe to our views.</p>
         {/* End Content */}
