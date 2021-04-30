219b1c41850c87306834717808fcff8761279fcb
munair <munair@gmail.com>
2021-04-30 18:46:25 +0900

    smaller heading for external links.

--- src/pages/Disclaimer.js
+++ src/pages/Disclaimer.js
@@ -10,7 +10,7 @@ class Disclaimer extends React.Component {
         <p>Useful Coin LLC assumes no responsibility for errors or omissions in the contents on the Service.</p>
         <p>In no event shall Useful Coin LLC be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Useful Coin LLC reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice.</p>
         <p>Useful Coin LLC does not warrant that the website is free of viruses or other harmful components.</p>
-        <h1>External links disclaimer</h1>
+        <h3>External links disclaimer</h3>
         <p><a href="http://www.usefulcoin.com" rel="nofollow">http://www.usefulcoin.com</a> website may contain links to external websites that are not provided or maintained by or in any way affiliated with Useful Coin LLC.</p>
         <p>Please note that the Useful Coin LLC does not guarantee the accuracy, relevance, timeliness, or completeness of any information on these external websites.</p>
       {/* End Content */}
