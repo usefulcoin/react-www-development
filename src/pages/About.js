0336f0d38e7f3e8e5ebeaea741166406bbfaf4cb
munair <munair@gmail.com>
2021-04-30 15:25:44 +0900

    not formatting correctly.

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,10 +11,10 @@ class About extends React.Component {
           <span className="date"><img src="images/usefulcoin-whiteicon-128x128px.png" width="10%" alt="brand icon" /></span>
           <h1 class="major" id="about">RESEARCH & STRATEGY</h1>
           <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
-            research and business development. That said, we have a penchant for collaborating teams in the cryptocurrency industry. Most of our
-            <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
-            engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just give us 
-            <a tel="+12062270634">call</a>.</p>
+            research and business development. That said, we have a penchant for collaborating teams in the cryptocurrency industry. Most of
+            our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
+            engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just give us <a tel="+12062270634">
+            call</a>.</p>
         </header>
         <ul className="actions special">
           <li><a mailto="info@usefulcoin.com" className="button large">Email Us</a></li>
