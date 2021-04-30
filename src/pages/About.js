497dd0f392aa93d2d36166b5cbeec98f7dfc1cb8
munair <munair@gmail.com>
2021-04-30 15:04:32 +0900

    add space

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ class About extends React.Component {
           <h1 class="major" id="about">RESEARCH & STRATEGY</h1>
           <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
             research and business development. That said, we have a penchant for collaborating teams in the cryptocurrency industry. Most of our
-            <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Contact us now via
+            <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Contact us now via 
             <a mailto="info@usefulcoin.com">email</a> to learn more about how we can be of service to you.</p>
         </header>
         <ul className="actions special">
