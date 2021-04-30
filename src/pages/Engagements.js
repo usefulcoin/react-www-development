d5b6b9aac973b9b01d284e9da737bb255c290dfe
munair <munair@gmail.com>
2021-04-30 16:49:05 +0900

    Add BIOP

--- src/pages/Engagements.js
+++ src/pages/Engagements.js
@@ -6,12 +6,15 @@ class Trade extends React.Component {
     <div>
       {/* Start Content */}
         <h1 className="major" id="engagements">Engagements</h1>
-        <p>We draft and execute thoughful marketing strategies, write impactful press releases, exhibit at major conferences, and develop educational content.
-           However, all-hands-on-deck <b>engagements</b> lasting three to nine month are our specialty. These engagements allow us to leverage our various
-           talents to produce the greatest impact.</p>
-        <p>Our most recent engagement is: </p>
-        <p>What started out as a pro bono community management engagement turned into an opportunity to <i>deeply embed</i> and join the founding team bootstrap
-           the Settlement DAO.</p>
+        <p>All-hands-on-deck <b>engagements</b> lasting three to nine month are our specialty. Although we
+           draft and execute thoughful marketing strategies, write impactful press releases, exhibit at major
+           conferences, and develop educational content, engagements allow us to leverage our various talents
+           and produce the greatest impact.</p>
+        <img src="images/biop-combination-mark-250x150px.png" alt="biop-combination-mark" />
+        <h3>The Settlement DAO</h3>
+        <p>What started out as a fun [pro bono] community management engagement turned into an opportunity to <i>deeply embed</i>.
+           Eventually, we joined the core developer to form a team to breathe life into the binary options settlement protocol
+           ("<b>BIOPSET</b>") and bootstrap its <b>Settlement DAO</b>.</p>
       {/* End Content */}
     </div>
   );
