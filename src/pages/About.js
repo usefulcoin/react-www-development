048f53a346c19778fe8c27f0fd5d389e39f50e76
munair <munair@gmail.com>
2021-04-30 18:24:16 +0900

    more options please.

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,8 +10,8 @@ class About extends React.Component {
         <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
           research and business development. That said, we have a penchant for collaborating with teams in the cryptocurrency industry. Most of
           our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
-          engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just give us <a href="tel:+12062270634">
-          call</a>.</p>
+          engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just <a href="tel:+12062270634">
+          call</a> or <a href="mailto:info@usefulcoin.com" className="button large">email</a> us.</p>
         <ul className="actions special">
           <li>
             <a href="mailto:info@usefulcoin.com" className="button large">Email Us</a>
