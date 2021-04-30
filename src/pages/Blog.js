36321f1ca94903156ed1b4ecafcaa59f266b6dd7
munair <munair@gmail.com>
2021-04-30 15:29:39 +0900

    cosmetic stuff

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,13 +3,13 @@ import React from 'react';
 class Trade extends React.Component {
   render() {
   return (
-    <React.Fragment>
-        {/* Start Content */}
+    <div>
+      {/* Start Content */}
         <h1 class="major" id="blog">Medium Publication</h1>
         <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> reflects our views on the industry:</p>
         <p>Please follow the publication if you subscribe to our views.</p>
-        {/* End Content */}
-    </React.Fragment>
+      {/* End Content */}
+    </div>
   );
   }
 }
