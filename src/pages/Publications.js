f306b164b9137fbaa4e58ca9192518c8b5b4f853
munair <munair@gmail.com>
2021-04-30 15:32:40 +0900

    tidying up some more.

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -3,12 +3,12 @@ import React from 'react';
 class Trade extends React.Component {
   render() {
   return (
-    <React.Fragment>
-        {/* Start Content */}
+    <div>
+      {/* Start Content */}
         <h1 class="major" id="publications">Publications</h1>
         <p>Coming soon.</p>
-        {/* End Content */}
-    </React.Fragment>
+      {/* End Content */}
+    </div>
   );
   }
 }
