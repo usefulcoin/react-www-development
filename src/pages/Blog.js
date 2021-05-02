b4c7a24ed8b32abaf8426440364d19263142cfd0
munair <munair@gmail.com>
2021-05-02 13:49:48 +0900

    clean up Blog... still needs UseEffects.

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 class Blog extends React.Component {
   componentDidMount() {
     window.mediumWidget();
@@ -7,13 +5,11 @@ class Blog extends React.Component {
 
   render() {
   return (
-    <div>
-      {/* Start Content */}
-        <h1 className="major" id="blog">Medium Publication</h1>
-        <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> occasionally reflects our views on the industry or aspects of an engagment. Take a look:</p>
-        <div id="medium-widget"></div>
-        <p>Please follow the publication if you subscribe to our views.</p>
-      {/* End Content */}
+    <div id="blog-content-wrapper">
+      <h1 className="major" id="blog">Medium Publication</h1>
+      <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> occasionally reflects our views on the industry or aspects of an engagment. Take a look:</p>
+      <div id="medium-widget"></div>
+      <p>Please follow the publication if you subscribe to our views.</p>
     </div>
   );
   }
