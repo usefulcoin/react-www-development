b09177f2107e07c60b5cc6e055913054f36bb4a2
munair <munair@gmail.com>
2021-05-02 18:12:41 +0900

    let's fit these buttons.

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -4,14 +4,14 @@ const Navigation = () => {
   return (
     <div id="navigation">
       <ul>
-        <li><Link className="button large" to="/">About</Link></li>
-        <li><Link className="button large" to="/Engagements">Engagements</Link></li>
-        <li><Link className="button large" to="/Talent">Talent</Link></li>
-        <li><Link className="button large" to="/Blog">Blog</Link></li>
-        <li><Link className="button large" to="/Publications">Publications</Link></li>
-        <li><Link className="button large" to="/Disclaimer">Disclaimer</Link></li>
-        <li><Link className="button large" to="/Privacy">Privacy Policy</Link></li>
-        <li><Link className="button large" to="/Terms">Terms & Conditions</Link></li>
+        <li><Link className="button large fit" to="/">About</Link></li>
+        <li><Link className="button large fit" to="/Engagements">Engagements</Link></li>
+        <li><Link className="button large fit" to="/Talent">Talent</Link></li>
+        <li><Link className="button large fit" to="/Blog">Blog</Link></li>
+        <li><Link className="button large fit" to="/Publications">Publications</Link></li>
+        <li><Link className="button large fit" to="/Disclaimer">Disclaimer</Link></li>
+        <li><Link className="button large fit" to="/Privacy">Privacy Policy</Link></li>
+        <li><Link className="button large fit" to="/Terms">Terms & Conditions</Link></li>
       </ul>
     </div>
   );
