8a3d576cca62a48e81b400ccfb09947a06b9b87e
munair <munair@gmail.com>
2021-05-02 18:14:33 +0900

    smaller buttons

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -4,14 +4,14 @@ const Navigation = () => {
   return (
     <div id="navigation">
       <ul>
-        <li><Link className="button large fit" to="/">About</Link></li>
-        <li><Link className="button large fit" to="/Engagements">Engagements</Link></li>
-        <li><Link className="button large fit" to="/Talent">Talent</Link></li>
-        <li><Link className="button large fit" to="/Blog">Blog</Link></li>
-        <li><Link className="button large fit" to="/Publications">Publications</Link></li>
-        <li><Link className="button large fit" to="/Disclaimer">Disclaimer</Link></li>
-        <li><Link className="button large fit" to="/Privacy">Privacy Policy</Link></li>
-        <li><Link className="button large fit" to="/Terms">Terms & Conditions</Link></li>
+        <li><Link className="button primary large fit small" to="/">About</Link></li>
+        <li><Link className="button primary large fit small" to="/Engagements">Engagements</Link></li>
+        <li><Link className="button primary large fit small" to="/Talent">Talent</Link></li>
+        <li><Link className="button primary large fit small" to="/Blog">Blog</Link></li>
+        <li><Link className="button primary large fit small" to="/Publications">Publications</Link></li>
+        <li><Link className="button primary large fit small" to="/Disclaimer">Disclaimer</Link></li>
+        <li><Link className="button primary large fit small" to="/Privacy">Privacy Policy</Link></li>
+        <li><Link className="button primary large fit small" to="/Terms">Terms & Conditions</Link></li>
       </ul>
     </div>
   );
