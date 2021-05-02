b22ca9756a8ae8e2852904dee436ad3fb69c6eaf
munair <munair@gmail.com>
2021-05-02 15:14:35 +0900

    last test.

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div id="nav-content-wrapper">
-      <ul className="links">
-        <li><Link to="/">About</Link></li>
-        <li><Link to="/Engagements">Engagements</Link></li>
-        <li><Link to="/Talent">Talent</Link></li>
-        <li><Link to="/Blog">Blog</Link></li>
-        <li><Link to="/Publications">Publications</Link></li>
-      </ul>
+      <nav id="nav">
+        <ul className="links">
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/Engagements">Engagements</Link></li>
+          <li><Link to="/Talent">Talent</Link></li>
+          <li><Link to="/Blog">Blog</Link></li>
+          <li><Link to="/Publications">Publications</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
