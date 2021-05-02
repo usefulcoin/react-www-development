2fe3d87eafb3c2b363b98b39b29615bd48b6307f
munair <munair@gmail.com>
2021-05-02 14:27:28 +0900

    update Nav

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   return (
     <div id="nav-content-wrapper">
-      <p><a href="http://www.usefulcoin.com" rel="nofollow">http://www.usefulcoin.com</a> Useful Coin</p>
+      <nav id="nav">
+        <ul className="links">
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/Engagements">Engagements</Link></li>
+          <li><Link to="/Talent">Talent</Link></li>
+          <li><Link to="/Blog">Blog</Link></li>
+          <li><Link to="/Publications">Publications</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
