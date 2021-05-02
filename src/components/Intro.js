bbd7e7b41d4c0119b7fe5c8bfe87e8bfed13a964
munair <munair@gmail.com>
2021-05-02 13:42:00 +0900

    update remaining components

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,5 @@
-import * as React from "react";
-
-
 const Intro = () => {
   return (
-    <div className="App">
-    {/* Start Intro */}
     <div id="intro">
       <img src="images/usefulcoin-whiteicon-128x128px.png" width="75px" height="75px" alt="white icon" />
       <h2>USEFUL COIN</h2>
@@ -12,8 +7,6 @@ const Intro = () => {
       <ul className="actions">
         <li><a href="#about" className="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
       </ul>
-    </div>
-    {/* End Intro */}
     </div>
   );
 }
