87455e316180a57660604aa8a9b74aab8b4b12c9
munair <munair@gmail.com>
2021-05-02 13:51:52 +0900

    clean up engagements content wrapper.

--- src/pages/Engagements.js
+++ src/pages/Engagements.js
@@ -1,25 +1,20 @@
-import React from 'react';
-
-
 const Engagements = () => {
   return (
-    <div>
-      {/* Start Content */}
-        <h1 className="major" id="engagements">Engagements</h1>
-        <p>All-hands-on-deck <b>engagements</b> lasting three to nine months are our specialty. Although we
-           draft and execute thoughful marketing strategies, write impactful press releases, exhibit at major
-           conferences, and develop educational content on demand, engagements really allow us to leverage our
-           various talents and produce the greatest impact.</p>
-        <span className="image left">
-          <a className="image dropshadow" href="https://www.biopset.com/">
-            <img src="images/biop-combination-mark-250x150px.png" alt="biop-combination-mark" />
-          </a>
-        </span>
-        <h3>The Settlement DAO</h3>
-        <p>What started out as a fun community management [pro bono] engagement turned into an opportunity to <i>deeply embed</i>.
-           Eventually, we joined the core developer to form a team to breathe life into the binary options settlement protocol
-           ("<b>BIOPSET</b>") and bootstrap its <b>Settlement DAO</b>.</p>
-      {/* End Content */}
+    <div id="engagements-content-wrapper">
+      <h1 className="major" id="engagements">Engagements</h1>
+      <p>All-hands-on-deck <b>engagements</b> lasting three to nine months are our specialty. Although we
+         draft and execute thoughful marketing strategies, write impactful press releases, exhibit at major
+         conferences, and develop educational content on demand, engagements really allow us to leverage our
+         various talents and produce the greatest impact.</p>
+      <span className="image left">
+        <a className="image dropshadow" href="https://www.biopset.com/">
+          <img src="images/biop-combination-mark-250x150px.png" alt="biop-combination-mark" />
+        </a>
+      </span>
+      <h3>The Settlement DAO</h3>
+      <p>What started out as a fun community management [pro bono] engagement turned into an opportunity to <i>deeply embed</i>.
+         Eventually, we joined the core developer to form a team to breathe life into the binary options settlement protocol
+         ("<b>BIOPSET</b>") and bootstrap its <b>Settlement DAO</b>.</p>
     </div>
   );
 }
