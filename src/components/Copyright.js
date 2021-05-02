42811f418bc4dd5aa2c16f38eb48777a74cf7597
munair <munair@gmail.com>
2021-05-02 13:37:45 +0900

    drop the comments since we have react components already separated.

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const Copyright = () => {
   return (
-    <div id="copyright"> {/* Start Copyright */}
+    <div id="copyright">
       <ul>
         <li>© {new Date().getFullYear()} USEFUL COIN LLC. ALL RIGHTS RESERVED.</li>
         <li><Link to="/Disclaimer">Disclaimer</Link></li>
         <li><Link to="/Privacy">Privacy Policy</Link></li>
         <li><Link to="/Terms">Terms & Conditions</Link></li>
       </ul>
-    </div> {/* End Copyright */}
+    </div>
   );
 }
 
