aab30ef69f0c4240921a506121c287715ef1fa63
munair <munair@gmail.com>
2021-05-02 13:54:22 +0900

    clean up

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,28 +1,23 @@
-import React from 'react';
-
-
 const Publications = () => {
   return (
-    <div>
-      {/* Start Content */}
-        <h1 className="major" id="publications">Publications</h1>
-        <h3>Coin Telegraph</h3>
-        <p>
-          <a href="https://cointelegraph.com/news/ico-token-rewards-policy-can-have-unintended-consequences-pose-dilemma-expert-blog">
-            https://cointelegraph.com/news/ico-token-rewards-policy-can-have-unintended-consequences-pose-dilemma-expert-blog
-          </a>
-        </p>
-        <p>
-          <a href="https://cointelegraph.com/news/correlation-between-cryptocurrency-value-and-exchange-listings-expert-blog">
-            https://cointelegraph.com/news/correlation-between-cryptocurrency-value-and-exchange-listings-expert-blog
-          </a>
-        </p>
-        <p>
-          <a href="https://cointelegraph.com/news/korea-s-unique-ability-to-make-its-reverse-ico-fad-permanent-expert-take">
-            https://cointelegraph.com/news/korea-s-unique-ability-to-make-its-reverse-ico-fad-permanent-expert-take
-          </a>
-        </p>
-      {/* End Content */}
+    <div id="publications-content-wrapper">
+      <h1 className="major" id="publications">Publications</h1>
+      <h3>Coin Telegraph</h3>
+      <p>
+        <a href="https://cointelegraph.com/news/ico-token-rewards-policy-can-have-unintended-consequences-pose-dilemma-expert-blog">
+          https://cointelegraph.com/news/ico-token-rewards-policy-can-have-unintended-consequences-pose-dilemma-expert-blog
+        </a>
+      </p>
+      <p>
+        <a href="https://cointelegraph.com/news/correlation-between-cryptocurrency-value-and-exchange-listings-expert-blog">
+          https://cointelegraph.com/news/correlation-between-cryptocurrency-value-and-exchange-listings-expert-blog
+        </a>
+      </p>
+      <p>
+        <a href="https://cointelegraph.com/news/korea-s-unique-ability-to-make-its-reverse-ico-fad-permanent-expert-take">
+          https://cointelegraph.com/news/korea-s-unique-ability-to-make-its-reverse-ico-fad-permanent-expert-take
+        </a>
+      </p>
     </div>
   );
 }
