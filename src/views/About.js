a43bce4a2ef9165c0cb6cfac742fff0ef10faa09
munair <munair@gmail.com>
2021-05-02 20:17:39 +0900

    update that copy to let people know how evolved you are.

--- src/views/About.js
+++ src/views/About.js
@@ -5,8 +5,9 @@ const About = () => {
     <div id="about-content-wrapper">
       <h1 class="major" id="about">COLLABORATION & ENGAGEMENT</h1>
       <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
-        research and business development. That said, we have a penchant for collaborating with teams in the cryptocurrency industry. Most of
-        our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
+        research and business development. That said, we focus on the cryptocurrency industry and outfits searching for a diligent and ethical
+        partner with multiple talents and an entrepreneurial mindset.<p>
+      <p>Most of our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
         engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just <a href="tel:+12062270634">
         call</a>/<a href="mailto:info@usefulcoin.com">email</a> us.</p>
       <ul className="actions special">
