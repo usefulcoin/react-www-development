961aa259e14c588585ee363e64520072e00d34d8
munair <munair@gmail.com>
2021-05-02 19:54:07 +0900

    test link and smoothscroll

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,3 +1,8 @@
+import {
+  Link,
+  animateScroll as scroll
+} from "react-scroll";
+
 const Intro = () => {
   return (
     <div id="intro">
@@ -5,7 +10,17 @@ const Intro = () => {
       <h2>USEFUL COIN</h2>
       <p>collaborative research & strategic engagement</p>
       <ul className="actions">
-        <li><a href="#about" className="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+        <li>
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            ><a href="#about" className="button icon solid solo fa-arrow-down scrolly">Continue</a>
+          </Link>
+        </li>
       </ul>
     </div>
   );
