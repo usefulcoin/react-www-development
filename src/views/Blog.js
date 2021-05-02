c5238d7a281fc30c591c79317602334656e5a884
munair <munair@gmail.com>
2021-05-02 20:44:26 +0900

    improve copy and CTA

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -9,9 +9,9 @@ class Blog extends React.Component {
   return (
     <div id="blog-content-wrapper">
       <h1 className="major" id="blog">Blog</h1>
-      <p>Our <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> occasionally reflects a view on industry or aspects of an engagment. Take a look:</p>
+      <p>The <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> occasionally reflects our views on industry or some aspect of an engagment. Take a look.</p>
       <div id="medium-widget"></div>
-      <p>Please follow the publication if you subscribe to our views.</p>
+      <p>Please follow the publication and share your feedback.</p>
     </div>
   );
   }
