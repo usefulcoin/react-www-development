16f612fbe59290366c94a3f27d12e57d6b71c301
munair <munair@gmail.com>
2021-05-02 09:12:56 +0900

    Header to function.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,16 @@
 import * as React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        {/* Start Header */}
-          <header id="header">
-            <img src="images/usefulcoin-whiteicon-128x128px.png" width="75px" height="75px" alt="white icon" />
-            <h2>USEFUL COIN</h2>
-          </header>
-        {/* End Header */}
-      </div>
-    );
-  }
+const Header = () => {
+  return (
+    <div className="App">
+      {/* Start Header */}
+        <header id="header">
+          <img src="images/usefulcoin-whiteicon-128x128px.png" width="75px" height="75px" alt="white icon" />
+          <h2>USEFUL COIN</h2>
+        </header>
+      {/* End Header */}
+    </div>
+  );
 }
 
 export default Header;
