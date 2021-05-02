d30852b3f6dbde49fddb50bad9969b7fef0c0e34
munair <munair@gmail.com>
2021-05-02 20:29:06 +0900

    nuff said

--- src/views/About.js
+++ src/views/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div id="about-content-wrapper">
       <h1 class="major" id="about">ABOUT</h1>
       <p>Useful Coin is a research and strategy consultancy based in Texas. We enjoy collaborating with orangizations of all stripes on marketing
-        research and business development. That said, we focus on the cryptocurrency industry and outfits searching for a diligent and ethical
+        research and strategic business development. That said, we focus on the cryptocurrency industry and outfits searching for a diligent and ethical
         partner to bring multiple talents and an entrepreneurial mindset to their endeavor.</p>
       <p>Most of our <b>engagements</b> tend to involve Korea and typically last between six to twelve months. Read about our <Link to="/Engagements">
         engagements</Link> to learn more about how we can be of service to you. Or save yourself some reading and just <a href="tel:+12062270634">
