c809cc97692117954564468ececc02be238dc449
munair <munair@gmail.com>
2021-05-02 09:00:13 +0900

    testing function for Copyright.

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,23 +2,19 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 
 
-class Copyright extends React.Component {
-  render() {
-    return (
-      <div className="App">
-      {/* Start Copyright */}
-        <div id="copyright">
-          <ul>
-            <li>© {new Date().getFullYear()} USEFUL COIN LLC. ALL RIGHTS RESERVED.</li>
-            <li><Link to="/Disclaimer">Disclaimer</Link></li>
-            <li><Link to="/Privacy">Privacy Policy</Link></li>
-            <li><Link to="/Terms">Terms & Conditions</Link></li>
-          </ul>
-        </div>
-      {/* End Copyright */}
+export default const Copyright = () => {
+  return (
+    <div className="App">
+    {/* Start Copyright */}
+      <div id="copyright">
+        <ul>
+          <li>© {new Date().getFullYear()} USEFUL COIN LLC. ALL RIGHTS RESERVED.</li>
+          <li><Link to="/Disclaimer">Disclaimer</Link></li>
+          <li><Link to="/Privacy">Privacy Policy</Link></li>
+          <li><Link to="/Terms">Terms & Conditions</Link></li>
+        </ul>
       </div>
-    );
-  }
+    {/* End Copyright */}
+    </div>
+  );
 }
-
-export default Copyright;
