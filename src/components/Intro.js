da12cf60d2184c4a114055ee5eb5f209ba290c4f
munair <munair@gmail.com>
2021-05-02 19:57:23 +0900

    it worked! holy moly. let's go!

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -16,7 +16,7 @@ const Intro = () => {
             to="about"
             spy={true}
             smooth={true}
-            offset={-70}
+            offset={-35}
             duration={500}
             ><a href="#about" className="button icon solid solo fa-arrow-down scrolly">Continue</a>
           </Link>
