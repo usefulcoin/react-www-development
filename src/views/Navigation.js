a2346e37531506bab320924e8ca8645bd438593b
munair <munair@gmail.com>
2021-05-03 16:27:57 +0900

    smallish bug

--- src/views/Navigation.js
+++ src/views/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
   return (
     <div id="navigation">
       <ul style={{listStyle: "none", margin: "0 0 2rem 0", padding: 0}}>
-        <li><Link className={buttonClasses} to="/">About</Link></li>
+        <li><Link className={buttonClasses} to="/Home">About</Link></li>
         <li><Link className={buttonClasses} to="/Engagements">Engagements</Link></li>
         <li><Link className={buttonClasses} to="/Talent">Talent</Link></li>
         <li><Link className={buttonClasses} to="/Blog">Blog</Link></li>
