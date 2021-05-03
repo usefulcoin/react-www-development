4506349dd629cb1fc18e33a4b6a04f4898497e59
munair <munair@gmail.com>
2021-05-03 18:29:50 +0900

    cleanup background color.

--- src/views/Navigation.js
+++ src/views/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = () => {
   return (
     <div id="navigation" style={{margin: 0, padding: 0}}>
       <ScrollToLink />
-      <ul style={{listStyle: "none", margin: "0 0 2rem 0", padding: 0}}>
+      <ul style={{listStyle: "none", margin: "0 0 2rem 0", padding: 0, backgroundColor: "#212931"}}>
         <li><Link className={buttonClasses} to="/Home">About</Link></li>
         <li><Link className={buttonClasses} to="/Engagements">Engagements</Link></li>
         <li><Link className={buttonClasses} to="/Talent">Talent</Link></li>
