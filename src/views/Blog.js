44e7b97b64db4c68cc778b43bf6ac05c5874691e
munair <munair@gmail.com>
2021-05-03 17:53:19 +0900

    test blog view scroll.

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,9 +1,19 @@
 import { useEffect } from 'react';
+import {
+  Link as ScrollToLink,
+  animateScroll
+} from "react-scroll";
 
 function Blog() {
-  useEffect( () => window.mediumWidget() );
+  useEffect(
+    () => {
+      animateScroll.scrollToTop()
+      window.mediumWidget()
+    }
+  );
   return (
     <div id="blog-content-wrapper">
+      <ScrollToLink />
       <h1 className="major" id="blog">Blog</h1>
       <p>The <a href="https://medium.com/usefulcoin" target="_medium">Medium Publication</a> occasionally reflects our views on industry or some aspect of an engagment. Take a look.</p>
       <div id="medium-widget"></div>
