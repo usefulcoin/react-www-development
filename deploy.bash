2d2b683a61ae913af5c03c5ac6d0ea07a7d7afdd
Ubuntu <ubuntu@ip-172-31-29-204.ap-northeast-2.compute.internal>
2021-05-14 22:13:48 +0000

    add deploy script

---
+++ deploy.bash
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# script name: deploy.bash
+# script author: munair simpson
+# script created: 20210515
+# script purpose: deploy react-www-development to gh-pages or S3 bucket
+
+read -p "Push to staging on gh-pages? (https enabled by default) [yes]: " staging
+read -p "Push to production on AWS S3? (https not enabled by default) [yes]: " production
+
+staging=${staging:-"yes"}
+production=${production:-"yes"}
+
+# step 1: push to GitHub Pages (**staging**)
+if [ $staging == "yes" ]; then git push origin main:gh-pages ; fi
+
+# step 2: copy to S3 bucket (**production**)
+if [ $production == "yes" ]; then aws s3 sync . s3://react-www-development ; fi
+
